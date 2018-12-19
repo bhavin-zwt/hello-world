af6cb6bc7f6fc402f9441f4bbf2f18729d8232ca
bhavin-zwt <45998022+bhavin-zwt@users.noreply.github.com>
2018-12-19 18:46:45 +0530

    My first file
    This is my first upload to this project

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+error_reporting('E_ALL');
+//send email
+echo mail("testing.eecms@gmail.com","My subject",'This is the test data');
+
+/*$zip = new ZipArchive;
+$res = $zip->open('Bosmac.zip');
+if ($res === TRUE) {
+  $zip->extractTo('data/');
+  $zip->close();
+  echo 'done';
+} else {
+  echo 'no';
+}*/
